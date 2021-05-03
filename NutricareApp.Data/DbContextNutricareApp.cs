db2505a5a59ce7aee5b017297d2806267e0553d2
Vetaman999 <68401851+Vetaman999@users.noreply.github.com>
2021-05-02 23:05:39 -0500

    actualizado
    add recipe, map, controller, model

--- NutricareApp.Data/DbContextNutricareApp.cs
+++ NutricareApp.Data/DbContextNutricareApp.cs
@@ -19,11 +19,14 @@ namespace NutricareApp.Data
 
         public DbSet<Nutritionist> Nutritionists { get; set; }
 
+        public DbSet<Recipe> Recipes { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfiguration(new ClientMap());
             modelBuilder.ApplyConfiguration(new NutritionistMap());
+            modelBuilder.ApplyConfiguration(new RecipeMap());
         }
     }
 }
--- NutricareApp.Entities/Nutritionist.cs
+++ NutricareApp.Entities/Nutritionist.cs
@@ -38,5 +38,7 @@ namespace NutricareApp.Entities
         [Required(ErrorMessage = "Debe incluir la fecha de creación del cliente")]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime CreatedAt { get; set; }
+
+        public virtual ICollection<Recipe> Recipes { get; set; }
     }
 }
