1e87472b721485e30e7e3e17e283a2068a135bbd
zloop1995 <zloop1995@gmail.com>
2021-05-03 22:15:53 -0500

    Revert "Merge pull request #4 from Soft-War-Web/Jose"
    This reverts commit b24529b7c7df42e6dea47b7f0bef2e98dd6a5962, reversing
    changes made to cd40dea24b9e121ccfbe4745310038152cddc922.

--- NutricareApp.Web/Controllers/ClientsController.cs
+++ NutricareApp.Web/Controllers/ClientsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using NutricareApp.Data;
 using NutricareApp.Entities;
+using NutricareApp.Web.Models;
 
 namespace NutricareApp.Web.Controllers
 {
@@ -23,14 +24,24 @@ namespace NutricareApp.Web.Controllers
 
         // GET: api/Clients
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Client>>> GetClients()
+        public async Task<IEnumerable<ClientModel>> GetClients()
         {
-            return await _context.Clients.ToListAsync();
+            var clientList = await _context.Clients.ToListAsync();
+            return clientList.Select(c => new ClientModel
+            {
+                ClientId = c.ClientId,
+                FirstName = c.FirstName,
+                LastName = c.LastName,
+                Password = c.Password,
+                Email = c.Email,
+                Username = c.Username,
+                CreatedAt = c.CreatedAt
+            });
         }
 
         // GET: api/Clients/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Client>> GetClient(int id)
+        [HttpGet("[action]/{id}")]
+        public async Task<IActionResult> GetClientById([FromRoute] int id)
         {
             var client = await _context.Clients.FindAsync(id);
 
@@ -39,70 +50,105 @@ namespace NutricareApp.Web.Controllers
                 return NotFound();
             }
 
-            return client;
+            return Ok(new ClientModel
+            {
+                ClientId = client.ClientId,
+                Username = client.Username,
+                Email = client.Email,
+                Password = client.Password,
+                FirstName = client.FirstName,
+                LastName = client.LastName,
+                CreatedAt = client.CreatedAt
+            });
         }
 
         // PUT: api/Clients/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutClient(int id, Client client)
+        [HttpPut("[action]")]
+        public async Task<IActionResult> PutClient([FromBody] UpdateClientModel model)
         {
-            if (id != client.ClientId)
-            {
-                return BadRequest();
-            }
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
 
-            _context.Entry(client).State = EntityState.Modified;
+            if (model.ClientId <= 0)
+                return BadRequest();
+
+            var client = await _context.Clients.FirstOrDefaultAsync(c => c.ClientId == model.ClientId);
+
+            if (client == null)
+                return NotFound();
+
+            client.FirstName = model.FirstName;
+            client.LastName = model.LastName;
+            client.Email = model.Email;
+            client.Username = model.Username;
+            client.Password = model.Password;
 
             try
             {
                 await _context.SaveChangesAsync();
             }
-            catch (DbUpdateConcurrencyException)
+            catch (DbUpdateConcurrencyException e)
             {
-                if (!ClientExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
+                return BadRequest(e.Message);
             }
 
-            return NoContent();
+            return Ok();
         }
 
         // POST: api/Clients
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Client>> PostClient(Client client)
+        public async Task<IActionResult> PostClient([FromBody] CreateClientModel model)
         {
-            _context.Clients.Add(client);
-            await _context.SaveChangesAsync();
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
 
-            return CreatedAtAction("GetClient", new { id = client.ClientId }, client);
+            Client client = new Client
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Username = model.Username,
+                Password = model.Password,
+                Email = model.Email
+            };
+
+            _context.Clients.Add(client);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+
+            return Ok();
         }
 
         // DELETE: api/Clients/5
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteClient(int id)
+        public async Task<IActionResult> DeleteClient([FromRoute] int id)
         {
             var client = await _context.Clients.FindAsync(id);
             if (client == null)
-            {
                 return NotFound();
-            }
 
             _context.Clients.Remove(client);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+            return Ok();
         }
 
         private bool ClientExists(int id)
         {
-            return _context.Clients.Any(e => e.ClientId == id);
+            return _context.Clients.Any(c => c.ClientId == id);
         }
     }
 }
