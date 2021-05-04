b46b0011781508039a398a9cd8addfb8c308ae05
Ravencl4vv <67107751+Ravencl4vv@users.noreply.github.com>
2021-05-03 21:00:51 -0500

    Creacion de los Map de Diet y Specialty

---
+++ NutricareApp.Data/Mapping/DietMap.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NutricareApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutricareApp.Data.Mapping
+{
+    public class DietMap : IEntityTypeConfiguration<Diet>
+    {
+        public void Configure(EntityTypeBuilder<Diet> builder)
+        {
+            builder.ToTable("diet")
+                .HasKey(c => c.DietId);
+            builder.Property(c => c.DietId)
+                .HasColumnName("diet_id");
+            builder.Property(c => c.DietName)
+                .HasColumnName("name")
+                .HasColumnType("varchar")
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            builder.Property(c => c.DietDescription)
+                .HasColumnName("description")
+                .HasColumnType("varchar")
+                .HasMaxLength(500)
+                .IsUnicode(false);
+            builder.Property(c=> c.CreatedAt)
+                .HasColumnName("created_at")
+                .HasColumnType("DateTime");
+        }
+    }
+}
