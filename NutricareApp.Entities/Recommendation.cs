5e6f690419c83100a0c733db954a60a5c50269dc
elmoebuta <55062521+elmoebuta@users.noreply.github.com>
2021-05-03 21:00:23 -0500

    recomendation commit 1

---
+++ NutricareApp.Entities/Recommendation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutricareApp.Entities
+{
+    public class Recommendation
+    {
+        public int RecommendationId { get; set; }
+
+        [Required(ErrorMessage = "Debe incluir el name del recommendation")]
+        [StringLength(16, MinimumLength = 6, ErrorMessage = "El name del recommendation debe tener de 5 a 50 caracteres")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Debe incluir la descripcion del recommendation")]
+        [StringLength(60, MinimumLength = 5, ErrorMessage = "La descripcion del recommendation debe tener menos de 250 caracteres")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Debe incluir la fecha de creación del recommendation")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        public string CreatedAt { get; set; }
+
+        [Required(ErrorMessage = "Debe incluir la fecha de la modificacion del recommendation")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        public string LastNotification { get; set; }
+
+}
