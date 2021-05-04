63cca3b6703a1f6e459ffa98478375314dfe10ee
elmoebuta <55062521+elmoebuta@users.noreply.github.com>
2021-05-04 01:34:39 -0500

    Jose5deMayo

---
+++ NutricareApp.Web/Models/RecommendationModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NutricareApp.Web.Models
+{
+    public class RecommendationModel
+    {
+        public int RecommendationId { get; set; }
+
+
+        public string Name { get; set; }
+
+
+        public string Description { get; set; }
+
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastModification { get; set; }
+    }
+}
