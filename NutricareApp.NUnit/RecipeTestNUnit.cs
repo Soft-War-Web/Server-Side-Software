7b09e9431d4e58196dff811380ae0a9829bc9238
Josue <josue.c1012@gmail.com>
2021-06-05 01:02:44 -0500

    UnitTests
    Recipe, professional profile, recommendation

--- NutricareApp.NUnit/RecipeTestNUnit.cs
+++ NutricareApp.NUnit/RecipeTestNUnit.cs
@@ -1,12 +1,76 @@
 ﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using NutricareApp.Data;
+using NutricareApp.Entities;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NutricareApp.Web.Controllers;
+using NutricareApp.Web.Models;
 using System.Threading.Tasks;
+using System;
 
 namespace NutricareApp.NUnit
 {
-    class RecipeTestNUnit
+    public class RecipeControllerTest
     {
+        private readonly DbContextOptionsBuilder<DbContextNutricareApp> _builder = new DbContextOptionsBuilder<DbContextNutricareApp>();
+        private readonly DbContextOptions<DbContextNutricareApp> _options;
+        private readonly List<Recipe> _recipes;
+
+        public RecipeControllerTest()
+        {
+            _builder.UseInMemoryDatabase("Test");
+            _options = _builder.Options;
+            Nutritionist nutritionist = new Nutritionist();
+            _recipes = new List<Recipe>
+            {
+                new Recipe { RecipeId = 1, NutritionistId = 1, CreatedAt = DateTime.Parse("2021-06-05T03:49:49.450Z"), Description = "receta de chaufa bajo en calorías", Favorite = 150, Ingredients = "ingredientes", LastModification = DateTime.Parse("2021-06-05T03:49:49.450Z"), Name = "Chaufa bajo en calorías", Preparation = "preparation"},
+                new Recipe { RecipeId = 2, NutritionistId = 2, CreatedAt = DateTime.Parse("2021-06-05T03:49:49.450Z"), Description = "receta de chaufa bajo en calorías", Favorite = 150, Ingredients = "ingredientes", LastModification = DateTime.Parse("2021-06-05T03:49:49.450Z"), Name = "Chaufa bajo en calorías", Preparation = "preparation"},
+                new Recipe { RecipeId = 3, NutritionistId = 3, CreatedAt = DateTime.Parse("2021-06-05T03:49:49.450Z"), Description = "receta de chaufa bajo en calorías", Favorite = 150, Ingredients = "ingredientes", LastModification = DateTime.Parse("2021-06-05T03:49:49.450Z"), Name = "Chaufa bajo en calorías", Preparation = "preparation"},
+                new Recipe { RecipeId = 4, NutritionistId = 4, CreatedAt = DateTime.Parse("2021-06-05T03:49:49.450Z"), Description = "receta de chaufa bajo en calorías", Favorite = 150, Ingredients = "ingredientes", LastModification = DateTime.Parse("2021-06-05T03:49:49.450Z"), Name = "Chaufa bajo en calorías", Preparation = "preparation"}
+            };
+        }
+
+        [Test]
+        public async Task GetRecipesAsyncReturnAIEnumarableOfRecipes()
+        {
+            using (var _context = new DbContextNutricareApp(_options))
+            {
+                _context.Recipes.AddRange(_recipes);
+                _context.SaveChanges();
+                var controller = new RecipesController(_context);
+
+                var result = await controller.GetRecipes();
+
+                Assert.True(typeof(IEnumerable<ProfessionalprofileModel>).IsInstanceOfType(result));
+                Assert.AreEqual(4, result.Count());
+            }
+        }
+
+        [Test]
+        public async Task GetRecipeByIdAsyncReturnARecipe()
+        {
+            using (var _context = new DbContextNutricareApp(_options))
+            {
+                CreateRecipeModel createRecipeModel = new CreateRecipeModel
+                {
+                    Name = "Chaufa",
+                    NutritionistId = 1,
+                    CreatedAt = DateTime.Parse("2021-06-05T03:49:49.450Z"),
+                    Description = "descripcion",
+                    Ingredients = "ingredientes",
+                    LastModification = DateTime.Parse("2021-06-05T03:49:49.450Z"),
+                    Preparation = "preparacion"
+                };
+                var controller = new RecipesController(_context);
+
+                var result = await controller.GetRecipeById(1);
+
+                Assert.True(typeof(ProfessionalprofileModel).IsInstanceOfType(result));
+            }
+        }
+
     }
 }
--- NutricareApp.NUnit/RecommendationTestNUnit.cs
+++ NutricareApp.NUnit/RecommendationTestNUnit.cs
@@ -1,12 +1,72 @@
 ﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using NutricareApp.Data;
+using NutricareApp.Entities;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NutricareApp.Web.Controllers;
+using NutricareApp.Web.Models;
 using System.Threading.Tasks;
 
 namespace NutricareApp.NUnit
 {
-    class RecommendationTestNUnit
+    public class RecommendationControllerTest
     {
+        private readonly DbContextOptionsBuilder<DbContextNutricareApp> _builder = new DbContextOptionsBuilder<DbContextNutricareApp>();
+        private readonly DbContextOptions<DbContextNutricareApp> _options;
+        private readonly List<Recommendation> _recommendations;
+
+        public RecommendationControllerTest()
+        {
+            _builder.UseInMemoryDatabase("Test");
+            _options = _builder.Options;
+            _recommendations = new List<Recommendation>
+            {
+                new Recommendation { RecommendationId = 1, NutritionistId = 1, Name = "recomendacion1", Description = "una recomendacion cualquiera", CreatedAt = DateTime.Parse("2021-06-05T03:49:49.450Z"), LastModification = DateTime.Parse("2021-06-05T03:49:49.450Z")},
+                new Recommendation { RecommendationId = 2, NutritionistId = 2, Name = "recomendacion2", Description = "una recomendacion cualquiera", CreatedAt = DateTime.Parse("2021-06-05T03:49:49.450Z"), LastModification = DateTime.Parse("2021-06-05T03:49:49.450Z")},
+                new Recommendation { RecommendationId = 3, NutritionistId = 3, Name = "recomendacion3", Description = "una recomendacion cualquiera", CreatedAt = DateTime.Parse("2021-06-05T03:49:49.450Z"), LastModification = DateTime.Parse("2021-06-05T03:49:49.450Z")},
+                new Recommendation { RecommendationId = 4, NutritionistId = 4, Name = "recomendacion4", Description = "una recomendacion cualquiera", CreatedAt = DateTime.Parse("2021-06-05T03:49:49.450Z"), LastModification = DateTime.Parse("2021-06-05T03:49:49.450Z")}
+            };
+        }
+
+        [Test]
+        public async Task GetRecommendationsAsyncReturnAIEnumarableOfRecommendations()
+        {
+            using (var _context = new DbContextNutricareApp(_options))
+            {
+                _context.Recommendations.AddRange(_recommendations);
+                _context.SaveChanges();
+                var controller = new RecommendationsController(_context);
+
+                var result = await controller.GetRecommendations();
+
+                Assert.True(typeof(IEnumerable<RecommendationModel>).IsInstanceOfType(result));
+                Assert.AreEqual(4, result.Count());
+            }
+        }
+
+        [Test]
+        public async Task GetRecommendationByIdAsyncReturnARecommendation()
+        {
+            using (var _context = new DbContextNutricareApp(_options))
+            {
+                CreateRecommendationModel createRecommendationModel = new CreateRecommendationModel
+                {
+                    Name = "recomendacion5",
+                    NutritionistId = 1,
+                    Description = "una recomendacion cualquiera",
+                    CreatedAt = DateTime.Parse("2021-06-05T03:49:49.450Z"),
+                    LastModification = DateTime.Parse("2021-06-05T03:49:49.450Z")
+                };
+                var controller = new RecommendationsController(_context);
+
+                var result = await controller.GetRecommendationById(1);
+
+                Assert.True(typeof(RecommendationModel).IsInstanceOfType(result));
+            }
+        }
+
     }
 }
